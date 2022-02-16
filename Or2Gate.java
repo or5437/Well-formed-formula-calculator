b62b5a12acdea6b356e201095d816111d62561e4
Or Hilo <or54377@gmail.com>
2022-02-17 00:14:39 +0200

    Delete Or2Gate.java

--- Or2Gate.java
+++
@@ -1,8 +0,0 @@
-package circuits;
-
-public class Or2Gate extends OrGate {
-	// constructor to initialize Or2Gate instance
-	public Or2Gate(Gate g1, Gate g2) {
-		super(new Gate[] { g1, g2 });
-	}
-}
